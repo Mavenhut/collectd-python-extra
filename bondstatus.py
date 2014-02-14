1c291361de9b750a7a667ea06e8c5e90daedc81b
Loic Lambiel <lll@exoscale.ch>
2014-02-14 23:50:20 +0100

    lacp collectd module

---
+++ bondstatus.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Loic Lambiel, exoscale
+
+
+def bond_status(intBondID):
+    try:
+        strBondPath = "/proc/net/bonding/bond%d" % intBondID
+        for line in open(strBondPath).readlines():
+            if "MII Status" in line:
+                strStatus = line.split(":")
+                strStatus = strStatus[1].strip()
+                if strStatus != "up":
+                    #bond nok
+                    intStatus = 1
+                    return intStatus,strStatus
+                else:
+                #bond ok
+                    intStatus = 0
+        return intStatus,strStatus
+    except:
+        return
+
+try:
+    import collectd
+
+    NAME = "Bonding"
+    VERBOSE_LOGGING = True
+
+    # logging function
+    def logger(t, msg):
+        if t == 'err':
+            collectd.error('%s: %s' % (NAME, msg))
+        elif t == 'warn':
+            collectd.warning('%s: %s' % (NAME, msg))
+        elif t == 'verb':
+            if VERBOSE_LOGGING:
+                collectd.info('%s: %s' % (NAME, msg))
+        else:
+            collectd.notice('%s: %s' % (NAME, msg))
+   
+
+    def read_callback():
+        i = 0
+        for i in range(0, 10):
+            intStatus = bond_status(i)
+            val = collectd.Values(plugin=NAME, type="gauge")
+            #val.plugin_instance = hostbill_stats['name']
+            val.values = intStatus
+            logger('verb', "Bond%s status is: %d" % i,intStatus)
+            val.type_instance = "Bond%s" % i
+            val.type = "gauge"
+            val.dispatch()
+
+
+except ImportError:
+    ## we're not running inside collectd
+    ## it's ok
+
+    i = 0
+    for i in range(0, 10):
+        try:
+            intStatus,strStatus = bond_status(i)
+            if intStatus == 0:
+                print "bond%d ok" % i
+            else:
+                print "bond%d error:%s" % i,strStatus
+        except:
+            continue
