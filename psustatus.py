30d76a3274abfd3c47f9c69f9af8527ab1514b5c
Loic Lambiel <lll@exoscale.ch>
2014-10-08 22:30:30 +0200

    collectd only script

--- psustatus.py
+++ psustatus.py
@@ -5,24 +5,24 @@
 
 
 
+import collectd
+import time
+from subprocess import PIPE, Popen
+global SLEEPTIME
+
+SLEEPTIME = 300
+VERBOSE_LOGGING = False
+NAME = "ipmi"
+
 def get_psustatus():
     psuStatus = {}
-    try:
-        logger('verb', "Performing ipmitool query to get psu status")
-    except:
-        pass
+    logger('verb', "Performing ipmitool query to get psu status")
+    
     try:
         p = Popen(["ipmitool", "sensor"], stdin=PIPE, stdout=PIPE, bufsize=1)
-        try:
-            logger('verb', "ipmitool query done")
-        except:
-            pass
+        logger('verb', "ipmitool query done")
     except:
-        try:
-            logger('err', "Failed to query ipmitool. Please ensure it is installed")
-        except:
-            pass
-
+        logger('err', "Failed to query ipmitool. Please ensure it is installed")
         raise
 
     for lines in p.stdout:
@@ -40,53 +40,35 @@ def get_psustatus():
     time.sleep(SLEEPTIME)        
     return psuStatus
 
-try:
-    import collectd
-    import time
-    from subprocess import PIPE, Popen
-    global SLEEPTIME
-    
-    SLEEPTIME = 300
-    VERBOSE_LOGGING = False
-
-    NAME = "ipmi"
-
-    # logging function
-    def logger(t, msg):
-        if t == 'err':
-            collectd.error('%s: %s' % (NAME, msg))
-        elif t == 'warn':
-            collectd.warning('%s: %s' % (NAME, msg))
-        elif t == 'verb':
-            if VERBOSE_LOGGING:
-                collectd.info('%s: %s' % (NAME, msg))
-        else:
-            collectd.notice('%s: %s' % (NAME, msg))
-   
-
-    def read_callback():
-        psu_status = get_psustatus()
-        val = collectd.Values(plugin=NAME, type="gauge")
-        if psu_status:
-            for key, value in psu_status.items():
-                val.values = [value]
-                logger('verb', "psu %s status is: %s" % (key, value))
-                val.type_instance =  key
-                val.type = "gauge"
-                val.dispatch()
-        else:
-            logger('verb', "no psu found")
-
-    collectd.register_read(read_callback) 
 
 
-except ImportError:
-    ## we're not running inside collectd
-    ## it's ok
+# logging function
+def logger(t, msg):
+    if t == 'err':
+        collectd.error('%s: %s' % (NAME, msg))
+    elif t == 'warn':
+        collectd.warning('%s: %s' % (NAME, msg))
+    elif t == 'verb':
+        if VERBOSE_LOGGING:
+            collectd.info('%s: %s' % (NAME, msg))
+    else:
+        collectd.notice('%s: %s' % (NAME, msg))
 
+
+def read_callback():
     psu_status = get_psustatus()
+    val = collectd.Values(plugin=NAME, type="gauge")
     if psu_status:
         for key, value in psu_status.items():
-            print "%s status is %s" % (key, value)
+            val.values = [value]
+            logger('verb', "psu %s status is: %s" % (key, value))
+            val.type_instance =  key
+            val.type = "gauge"
+            val.dispatch()
     else:
-        print "no psu found"
+        logger('verb', "no psu found")
+
+
+collectd.register_read(read_callback) 
+
+
